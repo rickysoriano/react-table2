bba88410030fcacc6c20e332df52ab6fed58eab5
rickysoriano <ricky_s01@hotmail.es>
2022-10-17 21:49:30 -0500

    estilos tabla

--- react-bootstrap-table-next/src/App.js
+++ react-bootstrap-table-next/src/App.js
@@ -1,34 +1,39 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import BootstrapTable from "react-bootstrap-table-next";
 
 function App() {
-  const [datos,setDatos] = useState([])
-  useEffect(() =>{
+  const [datos, setDatos] = useState([]);
+  useEffect(() => {
     getData();
-  },[])
+  }, []);
   const getData = () => {
-    axios("https://jsonplaceholder.typicode.com/comments").then((res)=>{
+    axios("https://jsonplaceholder.typicode.com/comments").then((res) => {
       setDatos(res.data);
-    })
-  }
-  const columnas =[
+    });
+  };
+ 
+  const columnas = [
     {
-      dataField:"email",
-      text:"Email"
+      dataField: "email",
+      text: "Email",
     },
     {
-      dataField:"name",
-      text:"Nombre"
+      dataField: "name",
+      text: "Nombre",
     },
     {
-      dataField:"body",
-      text:"Relleno"
+      dataField: "body",
+      text: "Relleno",
     },
-  ]
+    {
+      dataField: "postId",
+      text: "postId",
+    }
+  ];
   return (
     <div className="App">
-     <BootstrapTable keyField="id" data={datos} columns={columnas}/>
+      <BootstrapTable keyField="id" data={datos} columns={columnas} striped hover condensed/>
     </div>
   );
 }

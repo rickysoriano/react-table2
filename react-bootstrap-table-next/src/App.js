48b4cf4b8fb9ff073a289b28dec5aa44852b9e19
rickysoriano <ricky_s01@hotmail.es>
2022-10-17 21:38:22 -0500

    importar bootstrap

--- react-bootstrap-table-next/src/App.js
+++ react-bootstrap-table-next/src/App.js
@@ -1,7 +1,6 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 import BootstrapTable from "react-bootstrap-table-next";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [datos,setDatos] = useState([])

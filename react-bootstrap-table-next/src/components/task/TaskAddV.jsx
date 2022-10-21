1b0845b537619f414ed6196d250e08a4f7a73a1a
rickysoriano <ricky_s01@hotmail.es>
2022-10-20 22:33:38 -0500

    Linea2

--- react-bootstrap-table-next/src/components/task/TaskAddV.jsx
+++ react-bootstrap-table-next/src/components/task/TaskAddV.jsx
@@ -9,3 +9,19 @@ function App() {
   const [visible, setVisible] = useState(false);
   const [search, setSearch] = useState('');
   const [tasks, setTasks] = useState([]);
+  
+  const filterTask = (complete) => {
+    if(search){
+      const searchTask = tasks.filter(item => item.task.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+      return searchTask.filter(item => item.complete === complete)
+    }
+    return tasks.filter(item => item.complete === complete)
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center p-2"> Lista de tareas</h1>
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-5">
+            <input placeholder="Buscar tareas" onChange={event => setSearch(event.target.value)} className="form-control" />

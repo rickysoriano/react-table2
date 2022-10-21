913f13eb557525a0fb6f118d4140415af81ace73
rickysoriano <ricky_s01@hotmail.es>
2022-10-20 22:48:06 -0500

    export const

--- react-bootstrap-table-next/src/components/modal/Modal.jsx
+++ react-bootstrap-table-next/src/components/modal/Modal.jsx
@@ -1,3 +1,5 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+
+export const ModalForm =({open, setOpen, children})=> {

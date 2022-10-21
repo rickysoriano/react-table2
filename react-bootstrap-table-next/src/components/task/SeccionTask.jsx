eaac6cf3bf0db726dddfad377ad06ca42479fea6
rickysoriano <ricky_s01@hotmail.es>
2022-10-20 23:01:53 -0500

    Export SeccionTask

--- react-bootstrap-table-next/src/components/task/SeccionTask.jsx
+++ react-bootstrap-table-next/src/components/task/SeccionTask.jsx
@@ -1,3 +1,17 @@
 import React, { useState } from "react";
 import { ModalForm } from "./Modal";
 import { NewTask } from "./NewTask";
+export const SeccionTask = ({ task, setTasks, tasks }) => {
+    const [visible, setVisible] = useState(false);
+    const checkTask = (check) => {
+      const findTask = tasks.find((item) => item.id === task.id);
+      findTask.complete = check;
+      const filterTask = tasks.filter((item) => item.id !== task.id);
+      filterTask.push(findTask);
+      setTasks(filterTask);
+    };
+    const deleteTask = () => {
+      const filterTask = tasks.filter((item) => item.id !== task.id);
+      setTasks(filterTask);
+    };
+  

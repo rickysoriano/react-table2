0b8da5fd7c22d7dd7d3c9f54d73d1c26efbc5807
rickysoriano <ricky_s01@hotmail.es>
2022-10-20 22:49:45 -0500

    return modal

--- react-bootstrap-table-next/src/components/modal/Modal.jsx
+++ react-bootstrap-table-next/src/components/modal/Modal.jsx
@@ -1,5 +1,21 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
-export const ModalForm =({open, setOpen, children})=> {
+export const ModalForm = ({ open, setOpen, children }) => {
+  return (
+    <>
+      <Modal show={open} onHide={() => setOpen(false)}>
+        <Modal.Header closeButton>
+          {/*<Modal.Title>Crear tarea</Modal.Title>*/}
+        </Modal.Header>
+        <Modal.Body>{children}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setOpen(false)}>
+            Cerrar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};

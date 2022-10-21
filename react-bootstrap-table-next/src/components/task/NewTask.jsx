4b80fea56417f872e3871f4630f4d5d6fee37db0
rickysoriano <ricky_s01@hotmail.es>
2022-10-20 22:53:52 -0500

    const return

--- react-bootstrap-table-next/src/components/task/NewTask.jsx
+++ react-bootstrap-table-next/src/components/task/NewTask.jsx
@@ -1,3 +1,14 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+export const NewTask = ({ setOpen, setTasks, tasks, title }) => {
+    const [Task, setTask] = useState("");
+    const AccionTask = () => {
+      if (title) {
+        const findTask = tasks.find((item) => item.task === title);
+        findTask.task = Task;
+        const filterTask = tasks.filter((item) => item.task !== Task);
+        filterTask.push(findTask);
+        setTasks(filterTask);
+        setOpen(false);
+        return;
